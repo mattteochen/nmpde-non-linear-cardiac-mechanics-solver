e5614f613828dcb5c1b65b5067780b7d4b27c045
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2024-01-27 18:26:50 +0100

    Added AD to compute jacobian / documentation / clang formatter withgoogle style

--- src/tests/slab_cubic/main.cpp
+++ src/tests/slab_cubic/main.cpp
@@ -2,10 +2,12 @@
 
 int main(int argc, char* argv[]) {
   dealii::Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
-  const std::string  mesh_file_name = "../../../../lifex_fiber_generation_examples/mesh/slab_cubic.msh";
-  const unsigned int degree         = 1;
+  const std::string mesh_file_name =
+      "../../../../lifex_fiber_generation_examples/mesh/slab_cubic.msh";
+  const unsigned int degree = 1;
   SlabCubic s(mesh_file_name, degree);
   s.setup();
   s.solve_newton();
+  s.output();
   return 0;
 }
--- src/utils/Utils.hpp
+++ src/utils/Utils.hpp
@@ -3,12 +3,9 @@
 
 #include <deal.II/base/tensor.h>
 
-namespace Utils
-{
-namespace dealii
-{
-namespace Tensor
-{
+namespace Utils {
+namespace dealii {
+namespace Tensor {
 /**
  * Retrieve a dealii Tensor<2, dim> transpose
  * @tparam rank The dealii Tensor rank
@@ -17,10 +14,11 @@ namespace Tensor
  * @return The input tensor transpose
  */
 template <int rank = 2, int dim>
-::dealii::Tensor<rank, dim> get_transpose(const ::dealii::Tensor<rank, dim>& t) {
+::dealii::Tensor<rank, dim> get_transpose(
+    const ::dealii::Tensor<rank, dim>& t) {
   ::dealii::Tensor<rank, dim> transpose;
-  for (uint32_t i=0; i<dim; i++) {
-    for (uint32_t j=0; j<dim; j++) {
+  for (uint32_t i = 0; i < dim; i++) {
+    for (uint32_t j = 0; j < dim; j++) {
       transpose[j][i] = t[i][j];
     }
   }
@@ -36,15 +34,14 @@ template <int rank = 2, int dim>
 template <int rank = 2, int dim>
 const ::dealii::Tensor<rank, dim> get_identity() {
   ::dealii::Tensor<rank, dim> identity;
-  for (uint32_t i=0; i<dim; i++) {
+  for (uint32_t i = 0; i < dim; i++) {
     identity[i][i] = 1.0;
   }
   return identity;
 }
 
-}
-}
-}
+}  // namespace Tensor
+}  // namespace dealii
+}  // namespace Utils
 
-
-#endif 
+#endif
