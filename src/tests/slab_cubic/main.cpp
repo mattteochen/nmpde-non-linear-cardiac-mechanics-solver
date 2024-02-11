4eaf29d05f8f403efd41d8653602d5a5fdb27ac6
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2024-02-11 23:36:17 +0100

    Class refactoring / parameter handling

--- src/tests/slab_cubic/main.cpp
+++ src/tests/slab_cubic/main.cpp
@@ -4,8 +4,12 @@ int main(int argc, char* argv[]) {
   dealii::Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
   const std::string mesh_file_name =
       "../../../../lifex_fiber_generation_examples/mesh/slab_cubic.msh";
+  const std::string parameter_file_name =
+      "../parameters.prm";
+  const std::string problem_name = "slab_cubic";
   const unsigned int degree = 1;
-  SlabCubic s(mesh_file_name, degree);
+  SlabCubic<3, double> s(parameter_file_name, mesh_file_name, degree, problem_name);
+  s.initialise_boundaries_tag();
   s.setup();
   s.solve_newton();
   s.output();
